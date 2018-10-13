bac35dcfb850fd28ad52becc45a05c1149d0fae1
Vladimir Tiurin <bfg.tyurin@gmail.com>
2018-10-13 20:54:51 +0300

    add save contact in controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,15 @@
 class ContactsController < ApplicationController
-
   def new
-    
   end
 
   def create
-    render plain: params[:contact].inspect
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
   end
 end
